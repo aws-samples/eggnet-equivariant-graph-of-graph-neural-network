7f83e64d97dea4748c5f19b9ac4f6c43fd8fa163
Zichen Wang <zichewan@amazon.com>
2022-07-25 17:07:12 -0400

    Implemented NaturalComplexFeaturizer and corresponding PepBDB dataset

--- ppi/data.py
+++ ppi/data.py
@@ -8,6 +8,7 @@ from sklearn.metrics import pairwise_distances
 import torch
 import torch.utils.data as data
 import torch.nn.functional as F
+import dgl
 
 # custom modules
 from ppi.data_utils import remove_nan_residues
@@ -104,3 +105,57 @@ class PepBDBDataset(BasePPIDataset):
         g1 = self.featurizer1.featurize(parsed_structure["protein1"])
         g2 = self.featurizer2.featurize(parsed_structure["protein2"])
         return (g1, g2), y
+
+    def collate_fn(self, samples):
+        g1s, g2s = [], []
+        for (g1, g2), y in samples:
+            g1s.append(g1)
+            g2s.append(g2)
+            y = y.sum(axis=1) > 0  # interacting residues on protein1
+            y = torch.tensor(y, dtype=torch.float32)  # shape: (n_nodes, )
+            n_nodes = g1.num_nodes()
+            assert y.shape[0] == n_nodes
+            g1.ndata["target"] = y
+        return dgl.batch(g1s), dgl.batch(g2s)
+
+
+class PepBDBComplexDataset(BasePPIDataset):
+    """
+    Dataset representing PepBDB from http://huanglab.phys.hust.edu.cn/pepbdb/db/download/
+    Each entry contains a pair of polypeptide chains from a PDB complex.
+    This class featurize the protein complex as a whole.
+    """
+
+    def __init__(
+        self, data_list, contact_threshold=7.5, featurizer=None, **kwargs
+    ):
+        self.contact_threshold = contact_threshold
+        self.featurizer = featurizer
+        super(PepBDBComplexDataset, self).__init__(data_list, **kwargs)
+
+    def _preprocess(self, parsed_structure: dict):
+        coords_1 = np.asarray(parsed_structure["protein1"]["coords"])
+        coords_2 = np.asarray(parsed_structure["protein2"]["coords"])
+        # CA-CA distance:
+        contact_map = pairwise_distances(
+            coords_1[:, 1], coords_2[:, 1], metric="euclidean"
+        )
+        y = contact_map < self.contact_threshold
+        g = self.featurizer.featurize(parsed_structure)
+        return g, y
+
+    def collate_fn(self, samples):
+        """Collating protein complex graphs."""
+        graphs = []
+        for g, y in samples:
+            # interacting residues on protein1
+            y1 = torch.tensor(y.sum(axis=1) > 0, dtype=torch.float32)
+            # interacting residues on protein2
+            y2 = torch.tensor(y.sum(axis=0) > 0, dtype=torch.float32)
+            y = torch.cat((y1, y2))  # shape: (n_nodes, )
+            n_nodes = g.num_nodes()
+            assert y.shape[0] == n_nodes
+            g.ndata["target"] = y
+
+            graphs.append(g)
+        return dgl.batch(graphs)
