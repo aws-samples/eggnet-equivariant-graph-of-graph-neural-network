390017467e1f8d0048b6b02df04c8d7e2f19dc6a
Zichen Wang <zichewan@amazon.com>
2022-08-25 10:36:37 -0400

    Updated env setup script

--- setup_env.sh
+++ setup_env.sh
@@ -4,4 +4,5 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pytorch_p38
 
 conda install -y -c rdkit rdkit
+pip install dgl-cu111 dglgo -f https://data.dgl.ai/wheels/repo.html
 pip install -r requirements.txt
