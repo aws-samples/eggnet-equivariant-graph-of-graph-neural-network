51455330f3b43693d8b37675d66023c78ede5e35
Ryan Brand <rbrand1124@gmail.com>
2022-09-07 18:23:57 -0400

    Debug

--- ppi/modules.py
+++ ppi/modules.py
@@ -948,9 +948,9 @@ class GVPMultiStageEnergyModel(nn.Module):
 
         out_c_split = torch.split(out_c, protein_ligand_num_nodes)
         out_c_protein = out_c_split[::2]
-        print(len(out_c_protein))
+        print(out_c_protein[0].shape)
         out_c_ligand = out_c_split[1::2]
-        print(len(out_c_ligand))
+        print(out_c_ligand[0].shape)
 
         target_h = torch.stack(out_c_protein, dim=0)
         ligand_h = torch.stack(out_c_ligand, dim=0)
