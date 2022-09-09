129ba462e6dc33c9c0ddbcf2d26f440ae79cada8
Ryan Brand <rbrand1124@gmail.com>
2022-09-09 14:54:50 -0400

    Updated shell script

--- test_run.sh
+++ test_run.sh
@@ -2,35 +2,21 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pytorch_p38
     
-# # PDBBind dataset
-# python train.py --accelerator gpu \
-#     --max_epochs 1 \
-#     --precision 16 \
-#     --num_layers 2 \
-#     --dataset_name PDBBind \
-#     --model_name gvp \
-#     --input_type complex \
-#     --data_dir /home/ec2-user/SageMaker/efs/data/PIGNet/data/pdbbind_v2019/scoring \
-#     --num_workers 8 \
-#     --residual
-    
 # PDBBind dataset
 python train.py --accelerator gpu \
     --devices 1 \
-    --max_epochs 3000 \
+    --max_epochs 1 \
     --precision 16 \
-    --protein_num_layers 3 \
-    --ligand_num_layers 3 \
-    --complex_num_layers 3 \
-    --protein_node_h_dim 200 32 \
-    --protein_edge_h_dim 64 2 \
-    --ligand_node_h_dim 200 32 \
-    --ligand_edge_h_dim 64 2 \
-    --complex_node_h_dim 200 32 \
-    --complex_edge_h_dim 64 2 \
+    --stage1_num_layers 3 \
+    --stage2_num_layers 3 \
+    --stage1_node_h_dim 200 32 \
+    --stage1_edge_h_dim 64 2 \
+    --stage2_node_h_dim 200 32 \
+    --stage2_edge_h_dim 64 2 \
     --dataset_name PDBBind \
-    --input_type multistage-geometric-energy \
-    --model_name gvp-multistage-energy \
+    --input_type multistage-geometric \
+    --model_name multistage-gvp \
+    --use_energy_decoder \
     --data_dir /home/ec2-user/SageMaker/efs/data/PIGNet/data/pdbbind_v2019/scoring \
     --residual \
     --num_workers 8 \
@@ -40,4 +26,4 @@ python train.py --accelerator gpu \
     --loss_der1_ratio=10.0 \
     --loss_der2_ratio=10.0 \
     --min_loss_der2=-20.0 \
-    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/brandry/PDBBind_MSGVPEnergy_geometric \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/brandry/PDBBind_MSGVP_geometric \
