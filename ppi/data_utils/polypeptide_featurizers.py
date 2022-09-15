e68afae6a7bbe2cf5435f0cb690f549b089f5144
Ryan Brand <rbrand1124@gmail.com>
2022-09-15 12:26:49 -0400

    Hot fix

--- ppi/data_utils/polypeptide_featurizers.py
+++ ppi/data_utils/polypeptide_featurizers.py
@@ -1011,7 +1011,7 @@ class PIGNetHeteroBigraphComplexFeaturizerForEnergyModel(BaseFeaturizer):
                 res_mol = utils.residue_to_mol(res)
                 residue_smiles.append(Chem.MolToSmiles(res_mol))
                 for atom in res:
-                    sample["atom_to_residue"][atom.coord] = residue_counter
+                    sample["atom_to_residue"][[round(x, 5) for x in atom.coord.tolist()]] = residue_counter
                 residue_counter += 1
 
 
