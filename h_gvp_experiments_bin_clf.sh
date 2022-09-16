9d5b288f6493f8d54ce20cf1f16cf1f2aa768a07
Zichen Wang <zichewan@amazon.com>
2022-09-16 13:13:14 -0400

    Updated experiments

---
+++ h_gvp_experiments_bin_clf.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch_p38
+
+###########
+## Propedia
+###########
+python train.py --accelerator gpu \
+    --max_epochs 500 \
+    --precision 16 \
+    --num_layers 3 \
+    --node_h_dim 200 32 \
+    --edge_h_dim 64 2 \
+    --dataset_name Propedia \
+    --input_type complex \
+    --data_dir /home/ec2-user/SageMaker/efs/data/Propedia \
+    --residual \
+    --num_workers 8 \
+    --bs 32 \
+    --lr 1e-3 \
+    --early_stopping_patience 10 \
+    --residue_featurizer_name MACCS \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/zichen/Propedia_GVP_MACCS
+
+
+python train.py --accelerator gpu \
+    --devices 4 \
+    --max_epochs 2 \
+    --precision 16 \
+    --dataset_name Propedia \
+    --input_type complex \
+    --data_dir /home/ec2-user/SageMaker/efs/data/Propedia \
+    --residual \
+    --num_workers 16 \
+    --bs 16 \
+    --lr 1e-3 \
+    --early_stopping_patience 10 \
+    --residue_featurizer_name MACCS \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/zichen/Propedia_GVP_MACCS 
+
+
+python train.py --accelerator gpu \
+    --devices 4 \
+    --max_epochs 500 \
+    --precision 16 \
+    --dataset_name Propedia \
+    --input_type complex \
+    --data_dir /home/ec2-user/SageMaker/efs/data/Propedia \
+    --data_suffix small \
+    --residual \
+    --num_workers 4 \
+    --bs 16 \
+    --lr 1e-3 \
+    --early_stopping_patience 10 \
+    --residue_featurizer_name MACCS \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/zichen/Propedia_small_GVP_MACCS \
+    --persistent_workers True
+
+
+# less persistent workers to save CPU RAM
+# -> still may run out of CPU RAM
+python train.py --accelerator gpu \
+    --devices 4 \
+    --max_epochs 500 \
+    --precision 16 \
+    --num_layers 3 \
+    --node_h_dim 200 32 \
+    --edge_h_dim 64 2 \
+    --dataset_name Propedia \
+    --input_type complex \
+    --data_dir /home/ec2-user/SageMaker/efs/data/Propedia \
+    --residual \
+    --num_workers 8 \
+    --bs 16 \
+    --lr 1e-3 \
+    --early_stopping_patience 10 \
+    --residue_featurizer_name MACCS \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/zichen/Propedia_GVP_MACCS \
+    --persistent_workers True
+
+###########
+## PDBBind binary classification
+###########
+python train.py --accelerator gpu \
+    --devices 1 \
+    --max_epochs 500 \
+    --precision 16 \
+    --num_layers 3 \
+    --node_h_dim 200 32 \
+    --edge_h_dim 64 2 \
+    --dataset_name PDBBind \
+    --input_type complex \
+    --data_dir /home/ec2-user/SageMaker/efs/data/PIGNet/data/pdbbind_v2019_processed/scoring \
+    --residual \
+    --num_workers 4 \
+    --bs 32 \
+    --lr 1e-4 \
+    --early_stopping_patience 10 \
+    --residue_featurizer_name MACCS \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/zichen/PDBBind_bin_GVP_MACCS \
+    --persistent_workers True \
+    --binary_cutoff 6.7
+
+python train.py --accelerator gpu \
+    --devices 1 \
+    --max_epochs 500 \
+    --precision 16 \
+    --num_layers 3 \
+    --node_h_dim 200 32 \
+    --edge_h_dim 64 2 \
+    --dataset_name PDBBind \
+    --input_type complex \
+    --data_dir /home/ec2-user/SageMaker/efs/data/PIGNet/data/pdbbind_v2019_processed/scoring \
+    --residual \
+    --num_workers 4 \
+    --bs 32 \
+    --lr 1e-4 \
+    --early_stopping_patience 10 \
+    --residue_featurizer_name MolT5-small \
+    --default_root_dir /home/ec2-user/SageMaker/efs/model_logs/zichen/PDBBind_bin_GVP_MolT5_small \
+    --persistent_workers True \
+    --binary_cutoff 6.7
