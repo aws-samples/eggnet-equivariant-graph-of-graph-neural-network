378c0c77b2c03efaab9249dfa32572e03f8a58d1
Ryan Brand <rbrand1124@gmail.com>
2022-09-19 12:25:46 -0400

    Hot fix

--- ppi/modules.py
+++ ppi/modules.py
@@ -564,7 +564,7 @@ class MultiStageGVPModel(nn.Module):
                     else:
                         protein_atom_s = self.atomic_projections['Other'](protein_s[residue_idx, :])
                     # protein_atom_v = protein_v[residue_idx, :]
-                    protein_atom_v = coords
+                    protein_atom_v = coords.unsqueeze(1)
                     protein_atom_s_list.append(protein_atom_s)
                     protein_atom_v_list.append(protein_atom_v)
                     num_atoms += 1
