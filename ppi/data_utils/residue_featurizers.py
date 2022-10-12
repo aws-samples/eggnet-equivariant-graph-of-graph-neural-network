65b3a7472795215e0c8a417d254c45f2a67c7e82
Ryan Brand <rbrand1124@gmail.com>
2022-10-12 12:00:23 -0400

    Hot fix

--- ppi/data_utils/residue_featurizers.py
+++ ppi/data_utils/residue_featurizers.py
@@ -118,7 +118,7 @@ class GINFeaturizer(BaseResidueFeaturizer, nn.Module):
             node_feats = self.gin_model(bg, nfeats, efeats)
             graph_feats = self.readout(bg, node_feats)
         if single_str:
-            return graph_feats.unsqueeze(0)
+            return graph_feats.permute(1, 0)
         else:
             return graph_feats
 
