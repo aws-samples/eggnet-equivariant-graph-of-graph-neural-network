29737ee01a08709b4b660633a9eb17d31bb5f1dd
Zichen Wang <zichewan@amazon.com>
2022-10-13 10:20:59 -0400

    Complete integration tests

--- integration_test.sh
+++ integration_test.sh
@@ -27,3 +27,77 @@ python train.py --accelerator gpu \
     --data_dir $pdbbind_data
 
 # row4: pretrained GNN joint training	GVP	None
+python train.py --accelerator gpu \
+    --model_name hgvp \
+    --devices $n_gpus \
+    --fast_dev_run $n_gpus \
+    --precision 32 \
+    --dataset_name PDBBind \
+    --input_type complex \
+    --residue_featurizer_name MolT5-small-grad \
+    --data_dir $pdbbind_data
+
+# row5: pretrained GNN joint training	MS-GVP	None
+python train.py --accelerator gpu \
+    --model_name multistage-hgvp \
+    --devices $n_gpus \
+    --fast_dev_run $n_gpus \
+    --precision 32 \
+    --dataset_name PDBBind \
+    --input_type multistage-hetero \
+    --is_hetero \
+    --residue_featurizer_name MolT5-small-grad \
+    --data_dir $pdbbind_data
+
+# row6: pretrained GNN	GVP	E_int
+python train.py --accelerator gpu \
+    --model_name gvp \
+    --devices $n_gpus \
+    --fast_dev_run $n_gpus \
+    --precision 16 \
+    --dataset_name PDBBind \
+    --input_type complex \
+    --residue_featurizer_name MolT5-small \
+    --use_energy_decoder \
+    --is_hetero \
+    --data_dir $pdbbind_data
+
+# row7: pretrained GNN	MS-GVP	E_int
+python train.py --accelerator gpu \
+    --model_name multistage-gvp \
+    --devices $n_gpus \
+    --fast_dev_run $n_gpus \
+    --precision 16 \
+    --dataset_name PDBBind \
+    --input_type multistage-hetero \
+    --residue_featurizer_name MolT5-small \
+    --use_energy_decoder \
+    --is_hetero \
+    --data_dir $pdbbind_data
+
+# row8: pretrained GNN joint training	GVP	E_int
+python train.py --accelerator gpu \
+    --model_name hgvp \
+    --devices $n_gpus \
+    --fast_dev_run $n_gpus \
+    --precision 32 \
+    --dataset_name PDBBind \
+    --input_type complex \
+    --residue_featurizer_name MolT5-small-grad \
+    --use_energy_decoder \
+    --is_hetero \
+    --data_dir $pdbbind_data
+
+# row9: pretrained GNN joint training	MS-GVP	E_int
+python train.py --accelerator gpu \
+    --model_name multistage-hgvp \
+    --devices $n_gpus \
+    --fast_dev_run $n_gpus \
+    --precision 32 \
+    --dataset_name PDBBind \
+    --input_type multistage-hetero \
+    --residue_featurizer_name MolT5-small-grad \
+    --use_energy_decoder \
+    --is_hetero \
+    --data_dir $pdbbind_data
+    
