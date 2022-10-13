52daa29331f99477e7907c9a258ef027f9bb4080
Zichen Wang <zichewan@amazon.com>
2022-10-13 10:58:04 -0400

    Updated test variables

--- integration_test.sh
+++ integration_test.sh
@@ -2,8 +2,11 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pytorch_p38
 
+# global variables shared across all tests:
 n_gpus=4 
 pdbbind_data=/home/ec2-user/SageMaker/efs/data/PIGNet/data/pdbbind_v2019/scoring
+residue_featurizer_name=MolT5-small # to change this to pretrained GNN residue featurizer
+
 # row2: pretrained GNN	GVP	None
 python train.py --accelerator gpu \
     --model_name gvp \
@@ -12,7 +15,7 @@ python train.py --accelerator gpu \
     --precision 16 \
     --dataset_name PDBBind \
     --input_type complex \
-    --residue_featurizer_name MolT5-small \
+    --residue_featurizer_name $residue_featurizer_name \
     --data_dir $pdbbind_data
 
 # row3: pretrained GNN	MS-GVP	None 
@@ -23,7 +26,7 @@ python train.py --accelerator gpu \
     --precision 16 \
     --dataset_name PDBBind \
     --input_type multistage-hetero \
-    --residue_featurizer_name MolT5-small \
+    --residue_featurizer_name $residue_featurizer_name \
     --data_dir $pdbbind_data
 
 # row4: pretrained GNN joint training	GVP	None
@@ -34,7 +37,7 @@ python train.py --accelerator gpu \
     --precision 32 \
     --dataset_name PDBBind \
     --input_type complex \
-    --residue_featurizer_name MolT5-small-grad \
+    --residue_featurizer_name $residue_featurizer_name-grad \
     --data_dir $pdbbind_data
 
 # row5: pretrained GNN joint training	MS-GVP	None
@@ -46,7 +49,7 @@ python train.py --accelerator gpu \
     --dataset_name PDBBind \
     --input_type multistage-hetero \
     --is_hetero \
-    --residue_featurizer_name MolT5-small-grad \
+    --residue_featurizer_name $residue_featurizer_name-grad \
     --data_dir $pdbbind_data
 
 # row6: pretrained GNN	GVP	E_int
@@ -57,7 +60,7 @@ python train.py --accelerator gpu \
     --precision 16 \
     --dataset_name PDBBind \
     --input_type complex \
-    --residue_featurizer_name MolT5-small \
+    --residue_featurizer_name $residue_featurizer_name \
     --use_energy_decoder \
     --is_hetero \
     --data_dir $pdbbind_data
@@ -70,7 +73,7 @@ python train.py --accelerator gpu \
     --precision 16 \
     --dataset_name PDBBind \
     --input_type multistage-hetero \
-    --residue_featurizer_name MolT5-small \
+    --residue_featurizer_name $residue_featurizer_name \
     --use_energy_decoder \
     --is_hetero \
     --data_dir $pdbbind_data
@@ -83,7 +86,7 @@ python train.py --accelerator gpu \
     --precision 32 \
     --dataset_name PDBBind \
     --input_type complex \
-    --residue_featurizer_name MolT5-small-grad \
+    --residue_featurizer_name $residue_featurizer_name-grad \
     --use_energy_decoder \
     --is_hetero \
     --data_dir $pdbbind_data \
@@ -97,7 +100,7 @@ python train.py --accelerator gpu \
     --precision 32 \
     --dataset_name PDBBind \
     --input_type multistage-hetero \
-    --residue_featurizer_name MolT5-small-grad \
+    --residue_featurizer_name $residue_featurizer_name-grad \
     --use_energy_decoder \
     --is_hetero \
     --data_dir $pdbbind_data \
