1af1cb269b576f4f379cc399d63a1be2dee1885f
Ryan Brand <rbrand1124@gmail.com>
2022-10-25 14:04:01 -0400

    Hot fix

--- ppi/data_utils/polypeptide_featurizers.py
+++ ppi/data_utils/polypeptide_featurizers.py
@@ -1189,6 +1189,8 @@ class PIGNetHeteroBigraphComplexFeaturizerForEnergyModel(BaseFeaturizer):
         orientations = self._orientations(ca_coords)
         sidechains = self._sidechains(protein_residue_coords)
 
+        print(residues.shape)
+
         if self.residue_featurizer:
             node_s = torch.cat([dihedrals, residues], dim=-1)
         else:
