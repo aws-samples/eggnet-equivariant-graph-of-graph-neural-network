7ae102e7d89031832815464640b1190610e0a698
Zichen Wang <zichewan@amazon.com>
2022-12-08 15:56:19 -0500

    Updated python dependencies

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Setting up conda env for this project
+# Setting up conda env for this project 
+# Note: this requires pytorch_p38 in SageMaker or DLAMI
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pytorch_p38
 
-conda install -y -c rdkit rdkit
 pip install dgl-cu111 dglgo -f https://data.dgl.ai/wheels/repo.html
 pip install -r requirements.txt
